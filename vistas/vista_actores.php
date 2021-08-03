b616f30d89208bb3d32d39cea7c4fd5e5431231d
jose <juanjuancarlos24@hotmail.com>
2021-08-03 17:07:12 -0400

    modificacion

--- vistas/vista_actores.php
+++ vistas/vista_actores.php
@@ -1,64 +1,77 @@
-<?php require_once "vistas/partes/head.php"; ?>
+<?php require_once "partes/head.php"; ?>
 
 
-<div class="bg-light">
-    <div class="container">
-        <h1 class="display-3 py-5"><?php echo $pagina; ?></h1>
-    </div>
-</div>
-
-<form action="" method="post">
+<aside>
+        <div class="">
             <div class="container">
-                <label for="">Nombre</label>
-                <input type="text" name="nombre" class="form-control" placeholder="Ej: Fulanito.">
+                <h1 class="display-3 py-5"><?php echo $pagina; ?></h1>
             </div>
-            <div class="group-form container">
-                <label for="">Apellido</label>
-                <input name="apellido" type="text" class="form-control" placeholder="Ej: Guayacan.">
+        </div>
+        
+        <form action="" method="post">
+                    <div class="container">
+                        <label for="">Nombre</label>
+                        <input type="text" name="nombre" class="form-control" placeholder="Ej: Fulanito.">
+                    </div>
+                    <div class="group-form container">
+                        <label for="">Apellido</label>
+                        <input name="apellido" type="text" class="form-control" placeholder="Ej: Guayacan.">
+                    </div>
+                    <br>
+                    <div class= "container">
+                        <button name="guardar" class="btn btn-info">Guardar</button>
+                    </div>
+                </form>
+                <div class = "container"><?php echo $_SESSION['mensaje'] ?? ""; ?> </div>
+        <br>
+        <div class="container">
+            <div class="group-form">
+            <form class="formBuscar" action="">
+            <input name="nombre" type="text" class="" placeholder="Write Here">
+            <button name="buscar" class="btn btn-primary">Search</button>
+            </form>
             </div>
-            <br>
-            <div class= "container">
-                <button name="guardar" class="btn btn-info">Guardar</button>
+            
+        </div>
             </div>
-        </form>
-<br>
-<div class="container">
-    <div class="group-form">
-    <form action="">
-    <input name="nombre" type="text" class="form-control" placeholder="Write Here">
-    <button name="buscar" class="btn btn-primary">Search</button>
-    </form>
-    </div>
-    <div><?php echo $_SESSION['mensaje'] ?? ""; ?> </div>
-</div>
-    </div>
-</div>
-
-<hr>
-
-<div class="container">
-<table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Actor_id</th>
-      <th scope="col">First_name</th>
-      <th scope="col">Last_name</th>
-      <th scope="col">Last_update</th>
-    </tr>
-  </thead>
-  <tbody>
-      <?php
-        while($actor = mysqli_fetch_assoc($actores)){
-          echo "<tr>
-                    <th scope='row'>{$actor['actor_id']}</th>
-                    <td>{$actor['first_name']}</td>
-                    <td>{$actor['last_name']}</td>
-                    <td>{$actor['last_update']}</td>
-                </tr>";
-        }
-      ?>
-  </tbody>
-</table>
-</div>
+        </div>
+        
+        <hr>
+        
+        <div class="container">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Actor_id</th>
+              <th scope="col">First_name</th>
+              <th scope="col">Last_name</th>
+              <th scope="col">Last_update</th>
+            </tr>
+          </thead>
+          <tbody>
+              <?php
+                while($actor = mysqli_fetch_assoc($actores)){
+                  echo "<tr>
+                            <th scope='row'>{$actor['actor_id']}</th>
+                            <td>{$actor['first_name']}</td>
+                            <td>{$actor['last_name']}</td>
+                            <td>{$actor['last_update']}</td>
+                        </tr>";
+                }
+              ?>
+          </tbody>
+        </table>
+        </div>
+        
+      </aside>
+      <input type="checkbox" id="menu-toggle"/>
+        <label id="trigger" for="menu-toggle"></label>
+        <label id="burger" for="menu-toggle"></label>
+        <ul id="menu">
+          <li><a href="actores.php">Actores</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
 
 <?php require_once "vistas/partes/foot.php"; ?>
