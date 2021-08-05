b7378b78bd0bf32065f80bf2f40fbac590a44943
Jose <juanjuancarlos24@hotmail.com>
2021-08-05 11:20:36 -0400

    modificacion-menu

--- vistas/vista_actores.php
+++ vistas/vista_actores.php
@@ -4,14 +4,13 @@
   <label id="trigger" for="menu-toggle"></label>
   <label id="burger" for="menu-toggle"></label>
   <ul id="menu">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
-    <li><a href="#">Portfolio</a></li>
-    <li><a href="#">Contact</a></li>
+    <li><a href="actores.php">Actores</a></li>
+    <li><a href="categoria.php">Categorias</a></li>
+    <li><a href="pais.php">Pais</a></li>
+    <li><a href="lenguaje.php">Idiomas</a></li>
   </ul>
-
-<aside>
-        <div class="">
+<div class="web">
+<div class="">
             <div class="container">
                 <h1 class=""><?php echo $pagina; ?></h1>
             </div>
@@ -28,16 +27,16 @@
                     </div>
                     <br>
                     <div class= "container">
-                        <button name="guardar" class="btn btn-info">Guardar</button>
+                        <button name="guardar" class="adios">Guardar</button>
                     </div>
                 </form>
-                <div class = "container"><?php echo $_SESSION['mensaje'] ?? ""; ?> </div>
+                <div class = "bye container"><?php echo $_SESSION['mensaje'] ?? ""; ?> </div>
         <br>
         <div class="container">
             <div class="group-form">
             <form class="formBuscar" action="">
-            <input name="nombre" type="text" class="form-control" placeholder="Write Here">
-            <button name="buscar" class="btn btn-primary">Search</button>
+            <input name="nombre" type="text" class="hola" placeholder="Write Here">
+            <button name="buscar" class="boton">Search</button>
             </form>
             </div>
             
@@ -70,8 +69,9 @@
               ?>
           </tbody>
         </table>
-        </div>
+        </div>  
+</div>
+      
         
-      </aside>
 
 <?php require_once "vistas/partes/foot.php"; ?>
