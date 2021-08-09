ef4085f8340bd32e9f9a45a9cdbdbdd769c96ce5
Jose <juanjuancarlos24@hotmail.com>
2021-08-09 14:35:54 -0400

    index

--- index.php
+++ index.php
@@ -5,21 +5,6 @@ $pagina = "WikiSalami";
 
 
 
-if (isset($_POST['guardar'])) {
-$nombre = $_POST['nombre'] ?? "";
-$apellido = $_POST['apellido'] ?? "";
-
-$datos = [
-    'nombre' => $nombre,
-    'apellido' => $apellido
-];
-
-$insertado = insertarActor($conexion, $datos);
-
-if ($insertado) {
-    $_SESSION['mensaje'] = 'Datos insertados correctamente' ;
-}
-}
 
 
 require_once "vistas/vistas_index.php";
