9fccb55a9730809e88f7bd6a15ec8ba5f983e7bc
nika <nikaurypaulino20@gmail.com>
2021-08-09 21:35:05 -0400

    correccion

--- actores.php
+++ actores.php
@@ -4,40 +4,57 @@ require "modelos/modelo_actores.php";
 
 $pagina = "Actores";
 
-$actores = obtenerActores($conexion); 
+$actores = obtenerActores($conexion);
 
-if (isset($_GET['buscar'])) {
-    $nombre = $_GET['nombre'] ?? "";
-    $actores = obtenerActoresPorNombre($conexion, $nombre);
+try{
 
-    #var_dump($actores);
-}
-
-if (isset($_POST['guardar'])) {
-    $nombre = $_POST['nombre'] ?? "";
-    $apellido = $_POST['apellido'] ?? "";
+    if (isset($_GET['buscar'])) {
+        $nombre = $_GET['nombre'] ?? "";
+        $actores = obtenerActoresPorNombre($conexion, $nombre);
     
-    $datos = compact('nombre', 'apellido');
-    
-    $insertado = insertarActor($conexion, $datos);
-    
-    if ($insertado) {
-        $_SESSION['mensaje'] = 'Datos insertados correctamente' ;
-    } else {
-        $_SESSION['mensaje'] = 'Datos NO INSERTADOS' ;
+        #var_dump($actores);
     }
+    
+    
+    if (isset($_POST['guardar'])) {
+        $nombre = $_POST['nombre'] ?? "";
+        $apellido = $_POST['apellido'] ?? "";
+    
+        # validar los datos
+        if (empty($nombre)) {
+            throw new Exception("el nombre no puede estar vacio");
+        }
+    
+    
+    
+        if (empty($apellido)) {
+            throw new Exception("el apellido no puede estar vacio");
+        }
+        $datos = compact('nombre', 'apellido');
+    
+        $insertado = insertarActor($conexion, $datos);
+    
+        if ($insertado) {
+            $_SESSION['mensaje'] = 'Datos insertados correctamente';
+        } else {
+            $_SESSION['mensaje'] = 'Datos NO INSERTADOS';
+        }
     }
-
+    
     if (isset($_GET['editar'])) {
         $id = $_GET['editar'];
-
+    
         $resut = obtenerActoresPorId($conexion, $id);
-
+    
         $info = mysqli_fetch_assoc($result);
-      
-    
-    
     }
     
+
+} catch(Exception $ex) {
+$_SESSION['mensaje'] = $ex->getMessage();
+ 
+}
+
+
 #incluir la vista
 require_once 'vistas/vista_actores.php';
